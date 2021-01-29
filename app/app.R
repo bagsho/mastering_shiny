3d83884879ff83e82fef688a05ea5e031d1e1983
bagsho <oaktas@gmail.com>
2021-01-30 00:36:01 +0300

    come to Case study: ER injuries

--- app/app.R
+++ app/app.R
@@ -1,49 +1,17 @@
 library(shiny)
 
 ui <- fluidPage(
-    
-    sidebarLayout(
-        sidebarPanel(
-            numericInput("m", "Number of samples:", 2, min = 1, max = 100)
-        ),
-        mainPanel(
-            plotOutput("hist")
-        )
-    )
+    textInput("name", "What's your name?"),
+    textOutput("greeting")
 )
 
-ui <- fluidPage(
-    titlePanel("Central limit theorem"),
-    fluidRow(
-        column(4, 
-               sidebarPanel(
-                   numericInput("m", "Number of samples:", 2, min = 1, max = 100)
-               )
-        ),
-        column(8, 
-               sidebarPanel(
-                   numericInput("m", "Number of samples:", 2, min = 1, max = 100)
-               )
-        )
-    ),
-    fluidRow(
-        column(6, 
-               sidebarPanel(
-                   numericInput("m", "Number of samples:", 2, min = 1, max = 100)
-               )
-        ),
-        column(6, 
-               mainPanel(
-                   plotOutput("hist")
-        )
-    )
-))
-
 server <- function(input, output, session) {
-    output$hist <- renderPlot({
-        means <- replicate(1e4, mean(runif(input$m)))
-        hist(means, breaks = 20)
-    }, res = 96)
+    string <- reactive(paste0("Hello ", input$name, "!"))
+    
+    output$greeting <- renderText(string())
+    observeEvent(input$name, {
+        message("Greeting performed")
+    })
 }
 
 shinyApp(ui, server)
